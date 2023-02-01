bcc9751b4db77d533728376d3599e186f9541238
Matshiro <Daniel.B.Lewinski@gmail.com>
2023-02-01 14:45:33 +0100

    Added grid

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 let gridSize = 16;
+let brushColor = "#000000";
+let eraserColor = "#FFFFFF";
+
+const pixelContainer = document.getElementById('pixelContainer');
+
+document.addEventListener("DOMContentLoaded", function() {
+    createGrid();
+  });
 
 function createGrid(){
-    const newDiv = document.createElement("div");
-    
+
+    pixelContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    pixelContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr);`
+    for (let i = 0; i < gridSize * gridSize; i++){
+        const newDiv = document.createElement("div");
+        newDiv.style.backgroundColor = '#FFFFFF'
+        pixelContainer.appendChild(newDiv);
+    }
 }
